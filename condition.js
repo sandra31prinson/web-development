5972063237b84ca150607b5a1aa5a1b89be63aa6
sandra prinson <sandraprinson31@gmail.com>
2023-10-13 12:35:12 +0530

    if else condition

--- condition.js
+++ condition.js
@@ -19,4 +19,20 @@ var add = read.question('enter another number')
         console.log("highest number is"+add)
     }
 
+num1=read.question('number enter');
+num2=read.question('');
 
+console.log(num1,num2);
+
+ if(num1>num2)
+ {
+    console.log("1st is greater"+num1);
+ }
+ else if (num1===num2) {
+    console.log("it is same")
+    
+ } else {
+
+    console.log("the greatest is " + num2)
+    
+ }
