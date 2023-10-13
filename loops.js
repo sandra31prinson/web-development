277c2215e3925770a23c4ac0e7822a0107b71f77
sandra prinson <sandraprinson31@gmail.com>
2023-10-13 12:55:24 +0530

    for loop

---
+++ loops.js
@@ -0,0 +1,26 @@
+// for loop----
+
+for(i=2;i<=100;i=i+2)
+{
+    console.log(i);
+}
+
+for(i=0;i<=10;i++)
+{
+    console.log(i);
+}
+
+
+let n=5
+let String = ""
+for(i=0;i<=n;i++)
+{
+   
+    for(j=1;j<i+1;j++)
+    {
+        String+="*"
+    }
+
+    String+="\n"
+}
+console.log(String)
